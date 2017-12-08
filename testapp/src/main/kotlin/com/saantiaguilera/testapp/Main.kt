953be4ec36a72cb04cfa9e2396c368fb7ffcd99d
saantiaguilera <aguilerasanti@hotmail.com>
2017-12-08 01:43:57 -0300

    Add a test of a possible false positive

--- testapp/src/main/kotlin/com/saantiaguilera/testapp/Main.kt
+++ testapp/src/main/kotlin/com/saantiaguilera/testapp/Main.kt
@@ -6,25 +6,31 @@ class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            System.out.println("Black".black())
-            System.out.println("White".white())
-            System.out.println("Red".red())
-            System.out.println("Blue".blue())
-            System.out.println("Magenta".magenta())
-            System.out.println("Cyan".cyan())
+            // Show some colors
+            println("Black".black())
+            println("White".white())
+            println("Red".red())
+            println("Blue".blue())
+            println("Magenta".magenta())
+            println("Cyan".cyan())
 
-            System.out.println("Bold underline red on white"
+            // Create something tricky
+            println("Bold underline red on white"
                     .bold()
                     .underline()
                     .red() // This will get overwritten
                     .foreground(Colors.Color.WHITE)
                     .background(Colors.Color.RED))
 
+            // Enumerate all the colors we support
             val nOfColors = Colors.Color.values().size
-            System.out.println((1..nOfColors)
+            println((1..nOfColors)
                     .mapIndexed { i, num -> num.toString().colorize(Colors.Color.values()[i % nOfColors]) }
                     .map { colorizedString -> colorizedString.bold() }
                     .reduce { left, right -> "$left, $right" })
+
+            // Test that this is not a false positive
+            println("[34;33;32;31m".yellow())
         }
     }
 }
